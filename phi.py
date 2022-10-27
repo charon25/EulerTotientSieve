343f3ae80a6b2b0d94028bc117d0e53e1ea5f520
Paul Kern <pkern@laas.fr>
2022-10-27 11:51:44 +0200

    Small optimization

--- phi.py
+++ phi.py
@@ -3,7 +3,7 @@ import sys
 DEFAULT_LIMIT = 100000
 
 
-def compute_phi(LIMIT: int) -> list[int]:
+def compute_phi(LIMIT: int):# -> list[int]:
     """Compute phi(n) for all n in the interval [0, LIMIT) as a list, with the convention phi(0) = 0.
 
     Args:
@@ -11,10 +11,11 @@ def compute_phi(LIMIT: int) -> list[int]:
     """
     LIMIT = int(LIMIT)
 
-    # As we will work with successive multiplication, we need to initialize everything at 1
-    sieve = [n for n in range(LIMIT)]
+    # As we will work with successive "divisions", we need to initialize everything at n
+    # Small optimization : already divide every even number by 2 to avoir doing the longest inner loop
+    sieve = [n if n % 2 == 1 else n // 2 for n in range(LIMIT)]
 
-    for n in range(2, LIMIT):
+    for n in range(3, LIMIT):
         # If the sieve is still at its initial value, n is prime
         if sieve[n] != n:
             continue
@@ -29,9 +30,10 @@ def compute_phi(LIMIT: int) -> list[int]:
     return sieve
 
 
-
 if __name__ == '__main__':
     try:
         limit = int(sys.argv[1])
     except Exception:
         limit = DEFAULT_LIMIT
+
+    compute_phi(10000000)
