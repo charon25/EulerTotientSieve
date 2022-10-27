f6a7239632f8be9f56bdc34bb21b6f770aeb2e56
Paul Kern <pkern@laas.fr>
2022-10-27 11:28:07 +0200

    Chosen best function to compute phi

--- phi.py
+++ phi.py
@@ -3,41 +3,7 @@ import sys
 DEFAULT_LIMIT = 100000
 
 
-def compute_phi_1(LIMIT: int) -> list[int]:
-    """Compute phi(n) for all n in the interval [0, LIMIT) as a list, with the convention phi(0) = 0.
-
-    Args:
-        limit (_type_): Exclusive upper limit of the range.
-    """
-    LIMIT = int(LIMIT)
-
-    # As we will work with successive multiplication, we need to initialize everything at 1
-    sieve = [1 for _ in range(LIMIT)]
-    sieve[0] = 0
-
-    for n in range(2, LIMIT):
-        # If the sieve is still at one, n is prime
-        if sieve[n] != 1:
-            continue
-
-        # Propriety of phi : phi(p) = p - 1 for all primes p
-        sieve[n] = n - 1
-
-        for multiple in range(2 * n, LIMIT, n):
-            k = 0
-            m = multiple
-            while m % n == 0:
-                k += 1
-                m //= n
-
-            sieve[multiple] *= (n - 1) * (n ** (k - 1))
-
-
-    return sieve
-
-
-
-def compute_phi_2(LIMIT: int) -> list[int]:
+def compute_phi(LIMIT: int) -> list[int]:
     """Compute phi(n) for all n in the interval [0, LIMIT) as a list, with the convention phi(0) = 0.
 
     Args:
@@ -69,5 +35,3 @@ if __name__ == '__main__':
         limit = int(sys.argv[1])
     except Exception:
         limit = DEFAULT_LIMIT
-
-
--- test_phi.py
+++ test_phi.py
@@ -1,25 +1,23 @@
 import unittest
 
-from phi import compute_phi_1, compute_phi_2
+from phi import compute_phi
 
 
 class TestPhi(unittest.TestCase):
 
     def test_base_case(self):
-        self.assertListEqual(compute_phi_1(3), [0, 1, 1])
-        self.assertListEqual(compute_phi_2(3), [0, 1, 1])
+        self.assertListEqual(compute_phi(3), [0, 1, 1])
 
     def test_limit_50(self):
         CORRECT_VALUES = [0, 1, 1, 2, 2, 4, 2, 6, 4, 6, 4, 10, 4, 12, 6, 8, 8, 16, 6, 18, 8, 12, 10, 22, 8, 20, 12, 18, 12, 28, 8, 30, 16, 20, 16, 24, 12, 36, 18, 24, 16, 40, 12, 42, 20, 24, 22, 46, 16, 42]
-        self.assertListEqual(compute_phi_1(50), CORRECT_VALUES)
-        self.assertListEqual(compute_phi_2(50), CORRECT_VALUES)
+
+        self.assertListEqual(compute_phi(50), CORRECT_VALUES)
 
     def test_limit_100000(self):
         with open('test_phi_100000.txt', 'r') as fi:
             CORRECT_VALUES = list(map(int, fi.read().splitlines()))
 
-        self.assertListEqual(compute_phi_1(100000), CORRECT_VALUES)
-        self.assertListEqual(compute_phi_2(100000), CORRECT_VALUES)
+        self.assertListEqual(compute_phi(100000), CORRECT_VALUES)
 
 
 if __name__ == '__main__':
