ad2bf8b1aed59dae0eafa6adafd8d171db901c2d
Paul Kern <pkern@laas.fr>
2022-12-16 11:22:06 +0100

    Corrected some comments

--- phi.py
+++ phi.py
@@ -7,13 +7,10 @@ def compute_phi(LIMIT: int):# -> list[int]:
     """Compute phi(n) for all n in the interval [0, LIMIT) as a list, with the convention phi(0) = 0.
 
     This works using the expression : phi(n) = n * Σ (1 - 1 / p) for every prime p dividing n
-    This can be rewritten phi(n) = n * Σ (p - 1) / p
+    This can be rewritten as phi(n) = n * Σ (p - 1) / p
 
-    Then, instead of computing "n by n", we compute "p by p" :
-    go through every number n, and if it is prime, we go through every multiple k*n (k > 1) and multiply the sieve value by (n - 1) / n.
-    To avoid dealing with float numbers, we int divide sieve[k*n] by n (as it should be a multiple), and then multiply by (n - 1).
-
-    We we went through every number n, we in particular went through every prime number < LIMIT, and so we must have computed phi(n) for every n.
+    Then, instead of computing "n by n", we compute "p by p" : loop through every number n and if it is prime, loop through every multiple k*n (k > 1 and k*n < LIMIT) and multiply the sieve value by (n - 1) / n.
+    To avoid dealing with float numbers, we int divide sieve[k*n] by n (as it is a multiple), and then multiply by (n - 1).
 
     Args:
         limit (_type_): Exclusive upper limit of the range.
@@ -24,16 +21,15 @@ def compute_phi(LIMIT: int):# -> list[int]:
     sieve = [n for n in range(LIMIT)]
 
     for n in range(2, LIMIT):
-        # If the sieve is still at its initial value, n is prime
+        # If the sieve is not at its initial value, n is not a prime
         if sieve[n] != n:
             continue
 
-        # Propriety of phi : phi(p) = p - 1 for all primes p
+        # Property of phi : phi(p) = p - 1 for all primes p
         sieve[n] = n - 1
 
         for multiple in range(2 * n, LIMIT, n):
             sieve[multiple] = (sieve[multiple] // n) * (n - 1)
-
 
     return sieve
 
